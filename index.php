f115e9991d3d99caee2c39d7b71d8c2a7b3eb313
admin <admin@192.168.1.6>
2016-12-18 12:45:45 +0100

    Wyglad strony, linkowanie socialmedia

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TextTranslaredWeb</title>
+        
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        <link rel="stylesheet" href="css/fontello.css" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Slabo+27px&amp;subset=latin-ext" rel="stylesheet">
+    </head>
+    <body>
+        <div id="top">
+            <div id="log">Zaloguj się  |  Załóż konto</div>
+        </div>
+        <div id="box">
+            <div id="logo"><img src="img/tlo.png"/></div>
+            <div id="menu">
+                <div class="opcje">Strona główna</div>
+                <div class="opcje">Logowanie</div>
+                <div class="opcje">Kontakt</div>
+                <div class="opcje">Dodaj</div>
+                <div class="opcje">Tłumacz</div>
+                <div class="opcje2">Przetłumaczone</div>            
+            </div>
+
+            <div id="news">
+                <spam class="naglowki"> Strona głowna</spam>
+                <div class="linia"></div>
+                To jest strona główna
+                
+               
+            </div>
+
+            <div id="media">
+                <div id="mediaL"></div>
+                <div id="mediaR">
+                    <div class="fb">
+                        <a href="http://facebook.com" target="_blank" class="medialink"><i class="icon-facebook"></i></a>
+                    </div>
+                    <div class="yt">
+                        <a href="http://youtube.com" target="_blank" class="medialink"><i class="icon-youtube"></i></a>
+                    </div>
+                    <div class="gplus">
+                        <a href="http://plus.google.com" target="_blank" class="medialink"><i class="icon-gplus"></i></a>
+                    </div>
+                    <div class="tw">
+                        <a href="http://twitter.com" target="_blank" class="medialink"><i class="icon-twitter"></i></a>
+                    </div>
+                    <div style="clear: both;"></div>
+                </div>
+            </div>
+
+            <div id="stopka">Copyright &copy; by Przemysław Mackiewicz 2016-2017. Wszystkie prawa zastrzeżone.</div>
+        </div>
+        
+    </body>
+</html>
